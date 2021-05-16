12b094f788c0d6a46fe81242f7e149e232de8972
bribon-0 <bribon-0@student.ltu.se>
2021-05-16 21:51:38 +0200

    Fungerande Selenium Grid

--- Selenium Grid/src/grid/TestGrid.java
+++ Selenium Grid/src/grid/TestGrid.java
@@ -1,41 +1,33 @@
 package grid;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
+
 
 public class TestGrid {
     static WebDriver driver;
-    static String nodeURL:
+    static String nodeURL;
 
     public static void main(String[] args) {
+
         try {
-            node URL = "";
+            nodeURL = "http://100.24.234.88:4444/";
 
-            DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-            capabilities.setBrowserName("chrome");
-            capabilities.setPlatform(Platform.ANY);
-            driver = new RemoteWebDriver(new URL(nodeURL), capabilities);
-            driver.manage().deleteAllCookies();
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-            driver.manage().timeouts().pageLoadTimeout(45, TimeUnit.SECONDS);
+            ChromeOptions options = new ChromeOptions();
+            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+            desiredCapabilities.setPlatform(Platform.LINUX);
+            WebDriver driver = new RemoteWebDriver(new URL(nodeURL), options);
 
-            driver.get("http://www.google.com");
-            Thread.sleep(2000);
-            driver.findElement(By.cssSelector("#L2AGLb")).click();
+            driver.get("https://Test.Com");
+            System.out.println(driver.getCurrentUrl());
+        } catch (MalformedURLException e) {
 
-            //finds the search area
-            WebElement element = driver.findElement(By.name("q"));
-            //inserts text into search area
-            element.sendKeys("Software Testing");
-            Thread.sleep(2000);
-            element.sendKeys(Keys.ENTER);
-
-    }   catch (MalformedURLException e) {
             e.printStackTrace();
         }
     }
